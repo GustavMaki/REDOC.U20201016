698af96952968be3f89b1fbe6a8175beaece0353
GustavMaki <gustav.siljamaki@gmail.com>
2020-10-19 15:24:22 +0200

    update / PUT done

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -1,29 +1,33 @@
 document.querySelector("form").addEventListener("submit", e => {
    e.preventDefault();
 
-const queryString = window.location.href;
-console.log(queryString);
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('?');
-
 let name = document.getElementById("updateName").value;
 var price = document.getElementById("updatePrice").value;
 var description = document.getElementById("updateDescription").value;
 
-fetch('https://api.airtable.com/v0/appw9OO3eoSUmoKdt/Receipt%20Log?api_key=keyt35p1SiFUhVcYH', {
-method: "PUT",
-body: JSON.stringify({
-id: id,
-Name : name,
-Total : parseInt(price),
-Description : description
-}),
-headers: {
-'Content-type': 'application/json; charset=UTF-8'
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+id = urlParams.get('');
+
+document.querySelector("form").addEventListener("submit", e => {
+   e.preventDefault();
+
+url =`https://api.airtable.com/v0/appw9OO3eoSUmoKdt/Receipt%20Log/${id}`;
+
+const addData = {
+   "fields":{
+       "Name" : name,
+       "Total" : parseInt(price),
+       "Description" : description
+   }
 }
 
-})
+fetch (url, {method: 'PUT' , body: JSON.stringify(addData), 
+headers: {'Authorization': 'Bearer keyt35p1SiFUhVcYH', 'Content-Type': 'application/json'}})
 .then(response => response.json())
 .then(json => console.log(json))
+.catch(err => console.log (err));
+
+});
 
 });
